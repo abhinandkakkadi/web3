d6eae44a3f78980503b8c70345dbd78aacc5ac14
Abhinand Kakkadi <nanduttanvsabhi@gmail.com>
2025-06-22 10:41:32 +0530

    implemented web based wallets

--- 03-web-based-wallet/project/backend/go.mod
+++ 03-web-based-wallet/project/backend/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
 	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.5 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.6 // indirect
 	github.com/consensys/bavard v0.1.27 // indirect
 	github.com/consensys/gnark-crypto v0.16.0 // indirect
 	github.com/crate-crypto/go-eth-kzg v1.3.0 // indirect
@@ -16,6 +18,7 @@ require (
 	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/supranational/blst v0.3.14 // indirect
 	github.com/tyler-smith/go-bip32 v1.0.0 // indirect
 	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
--- 03-web-based-wallet/project/backend/main.go
+++ 03-web-based-wallet/project/backend/main.go
@@ -1,67 +1,132 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"slices"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/mr-tron/base58"
 	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
 )
 
-var (
-	index uint32 = 0
+type Code string
+
+const (
+	Ethereum Code = "ethereum"
+	Solana   Code = "solana"
+	Bitcoin  Code = "bitcoin"
 )
 
-type Wallet struct {
-	PublicAddress string
+var supportedCoin = []Code{
+	Ethereum, Solana,
 }
 
-var walletMap = map[uint32]*Wallet{}
+var Mnemonic *string
+
+type Wallet struct {
+	PublicAddress string `json:"address"`
+}
+
+var walletMap = map[Code][]*Wallet{}
+
+var coinIndexMap = map[Code]*CoinDetail{
+	Ethereum: {
+		Index: 60,
+	},
+	Solana: {
+		Index: 501,
+	},
+	Bitcoin: {
+		Index: 0,
+	},
+}
+
+type GenerateAdderss func(key *bip32.Key) (string, error)
+
+var GenerateAddressMap = map[Code]GenerateAdderss{
+	Ethereum: generateEthereumAddress,
+	Solana:   generateSolanaAddress,
+}
+
+type CoinDetail struct {
+	Index        uint32
+	CurrentIndex uint32
+}
 
 func main() {
-	http.HandleFunc("/", generateAddress)
+	http.Handle("/", headerMiddleware(http.HandlerFunc(fetchWallet)))
+	http.Handle("/generate", headerMiddleware(http.HandlerFunc(generateAddress)))
 	http.ListenAndServe(":8080", nil)
 }
 
-func generateAddress(w http.ResponseWriter, r *http.Request) {
+func getMnemonic() string {
+	if Mnemonic != nil {
+		return *Mnemonic
+	}
+
 	entropy, err := bip39.NewEntropy(128)
 	if err != nil {
-		http.Error(w, "Failed to generate entropy", http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 
 	mnemonic, err := bip39.NewMnemonic(entropy)
 	if err != nil {
-		http.Error(w, "Failed to generate mnemonic", http.StatusInternalServerError)
+		panic(err)
+	}
+
+	Mnemonic = &mnemonic
+
+	return mnemonic
+}
+
+func initWallet(coin Code) {
+	if walletMap[coin] == nil {
+		walletMap[coin] = []*Wallet{}
+	}
+}
+
+func headerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func fetchWallet(w http.ResponseWriter, _ *http.Request) {
+	json.NewEncoder(w).Encode(walletMap)
+}
+
+func generateAddress(w http.ResponseWriter, r *http.Request) {
+	coin := Code(r.URL.Query().Get("coin"))
+	if !slices.Contains(supportedCoin, coin) {
+		http.Error(w, "invalid_coin", http.StatusInternalServerError)
 		return
 	}
 
-	seed := bip39.NewSeed(mnemonic, "")
+	initWallet(coin)
+
+	seed := bip39.NewSeed(getMnemonic(), "")
 
 	masterKey, err := bip32.NewMasterKey(seed)
 	if err != nil {
-		http.Error(w, "Failed to generate master key", http.StatusInternalServerError)
+		http.Error(w, "failed_to_generate_master_key", http.StatusInternalServerError)
 		return
 	}
 
-	purpose, _ := masterKey.NewChildKey(hardenedIndex(44))
-	coinType, _ := purpose.NewChildKey(hardenedIndex(60))
-	account, _ := coinType.NewChildKey(hardenedIndex(0))
-	change, _ := account.NewChildKey(0)
-	addressIndex, _ := change.NewChildKey(index)
+	addressIndex, _ := getChildKey(masterKey, coin)
 
-	privateKey, err := crypto.ToECDSA(addressIndex.Key)
-	if err != nil {
-		http.Error(w, "Failed to generate master key", http.StatusInternalServerError)
-		return
-	}
+	address, _ := GenerateAddressMap[coin](addressIndex)
 
-	address := crypto.PubkeyToAddress(privateKey.PublicKey)
-	walletMap[index] = &Wallet{
-		PublicAddress: address.Hex(),
-	}
-	index++
+	walletMap[coin] = append(walletMap[coin], &Wallet{
+		PublicAddress: address,
+	})
+
+	fmt.Println(walletMap)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(walletMap)
@@ -69,4 +134,33 @@ func generateAddress(w http.ResponseWriter, r *http.Request) {
 
 func hardenedIndex(index uint32) uint32 {
 	return index + bip32.FirstHardenedChild
+}
+
+func getChildKey(masterKey *bip32.Key, coin Code) (*bip32.Key, error) {
+	purpose, _ := masterKey.NewChildKey(hardenedIndex(44))
+	coinType, _ := purpose.NewChildKey(hardenedIndex(coinIndexMap[coin].Index))
+	account, _ := coinType.NewChildKey(hardenedIndex(0))
+	change, _ := account.NewChildKey(0)
+
+	coinIndexMap[coin].CurrentIndex++
+
+	return change.NewChildKey(coinIndexMap[coin].CurrentIndex)
+}
+
+func generateSolanaAddress(key *bip32.Key) (string, error) {
+	hash := sha256.Sum256(key.Key)
+
+	pubKey := hash[:32]
+
+	return base58.Encode(pubKey), nil
+}
+
+func generateEthereumAddress(key *bip32.Key) (string, error) {
+	privateKey, err := crypto.ToECDSA(key.Key)
+	if err != nil {
+		return "", err
+	}
+
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	return address.Hex(), nil
 }
