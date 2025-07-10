e79752167bcc9b50e3303be5d3b6d628d7c7f9df
Abhinand Kakkadi <nanduttanvsabhi@gmail.com>
2025-07-10 22:11:10 +0530

    implement create transaction

--- 05-solana-dapp/dapp/src/Airdrop.jsx
+++ 05-solana-dapp/dapp/src/Airdrop.jsx
@@ -1,25 +1,101 @@
 // components in react 
 
+// TODO: refactor this code with design enhancements
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { useEffect, useState } from "react";
+import bs58 from "bs58";
+import { SystemProgram, Transaction, PublicKey } from "@solana/web3.js";
 
 export function Airdrop() {
     const wallet = useWallet()
     const { connection } = useConnection();
+
     const [amount, setAmount] = useState("");
+    const [txnAmount, setTxnAmount] = useState("");
+    const [recipientAddress, setRecipientAddress] = useState("");
+
+    const [airdropMessage, setAirdropMessage] = useState(null);
+    const [walletBalance, setWalletBalance] = useState(0);
+    const [txnId, setTxnId] = useState(null);
+
+    useEffect(()=>{
+        if (wallet.publicKey) {
+                connection.getBalance(wallet.publicKey).then((balance) => {
+                    setWalletBalance(balance/1000000000)
+        })
+        } else {
+            setWalletBalance(0);  
+        }
+    }, [connection, wallet.publicKey, airdropMessage])
+
+    async function sendTransactionToRecipient() {
+        if (!wallet.publicKey || !wallet.signTransaction) {
+            throw new Error("Wallet not connected or signTransaction not available");
+        }
+
+        const recepient = new PublicKey(recipientAddress)
+        const lamports = txnAmount * 1e9; 
+
+        const transaction = new Transaction().add(SystemProgram.transfer({
+            fromPubkey: wallet.publicKey,
+            toPubkey: recepient,
+            lamports: lamports,
+        }))
+
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        const signed = await wallet.signTransaction(transaction);
+        const txid = await connection.sendRawTransaction(signed.serialize());
+        await connection.confirmTransaction(txid);
+        setTxnId(txid);
+    }
+
+    async function signMessage() {
+       try {
+            const message = new TextEncoder().encode("Hello, Solana!");
+            const signature = await wallet.signMessage(message);
+            if (signature) {
+                setTxnId(bs58.encode(signature));
+            } else {
+                setTxnId("No signature returned");
+            }
+            
+       } catch (error) {
+            console.log("Error signing message:", error);
+       }
+    }
 
     async function sendAirdropToUser() {
         try {
-            console.log("Sending airdrop to:",  amount * 1e9);
-            await connection.requestAirdrop(wallet.publicKey, amount * 1e9); 
-            alert("Airdrop sent successfully!")
+            if (!wallet.publicKey) {
+                console.log("Wallet not connected");
+                return
+            }
+
+            await connection.requestAirdrop(wallet.publicKey, Number(amount) * 1e9); 
+            setAirdropMessage("Airdrop sent successfully!");
         } catch (error) {
             console.error("Error sending airdrop:", error);
-            alert("Failed to send airdrop. Please check the console for details.");
+            const jsonString = error.message.slice(error.message.indexOf('{'));
+            const obj = JSON.parse(jsonString);
+            setAirdropMessage(obj?.error?.message);
         }
     }
 
-    return <div>
-        <input type="text" placeholder="amount" value={amount} onChange={(e)=> setAmount(e.target.value)}></input>
-        <button onClick={sendAirdropToUser}>Send Airdrop</button>
-    </div>
+    return (
+    <div>
+        <input className="border rounded m-4 p-2" type="text" placeholder="amount" value={amount} onChange={(e)=> setAmount(e.target.value)}></input>
+        <button onClick={sendAirdropToUser} className="border rounded m-4 p-2">Send Airdrop</button>
+        <div>SOL Balance: {walletBalance}</div>
+        {/* {wallet.publicKey && <button onClick={signMessage} className="border rounded m-4 p-2">Sign Message</button>} */}
+        {wallet.publicKey && <div className="flex items-center justify-center">
+            <input className="border rounded m-4 p-2" type="text" placeholder="amount" value={txnAmount} onChange={(e)=> setTxnAmount(e.target.value)}></input>
+            <input className="border rounded m-4 p-2" type="text" placeholder="recepient" value={recipientAddress} onChange={(e)=> setRecipientAddress(e.target.value)}></input>
+            <button onClick={sendTransactionToRecipient} className="border rounded m-4 p-2">Send Transaction</button>
+            {txnId && <div>Transaction id: {txnId}</div>}
+            </div>}
+        
+        {airdropMessage && <div className="font-bold underline">{airdropMessage}</div>}
+    </div>)  
 }
--- 05-solana-dapp/dapp/src/App.jsx
+++ 05-solana-dapp/dapp/src/App.jsx
@@ -14,13 +14,18 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 function App() {
   return (
     <ConnectionProvider endpoint={"https://api.devnet.solana.com"} aut>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
             <WalletProvider wallets={[]}> {/* In the right now empty list we can add wallets that cannot be automatically detected */ }
                 <WalletModalProvider autoConnect>
+                  <p className='mb-2 font-bold'>SOL Faucet</p>
+                  <div className='flex flex items-center justify-center gap-2'>
                   <WalletMultiButton/>
                   <WalletDisconnectButton/>
+                  </div>
                   <Airdrop/>
                 </WalletModalProvider>
             </WalletProvider>
+      </div>
     </ConnectionProvider>
   )
 }
